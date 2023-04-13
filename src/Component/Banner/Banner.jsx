2819bcc5f36243e79fd3215242da00c90107a842
Rakibul Islam <rakibulrakib05@gmail.com>
2023-04-14 05:57:07 +0600

    make job category and feature

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-//import JobCtegory from '../../JobCategory/JobCategory';
-//import Feature from '../../Feature/Feature';
+import JobCtegory from '../../Component/JobCategory/JobCategory';
+import Feature from '../../Component/Feature/Feature';
 
 const Banner = () => {
     return (
@@ -16,7 +16,8 @@ const Banner = () => {
                     <button className="btn text-white bg-gradient-to-br from-[#7E90FE] to-[#9873FF] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-3 ">Get Started</button>
                 </div>
             </div>
-            
+            <JobCtegory></JobCtegory>
+            <Feature></Feature>
         </div>
     );
 };
---
+++ src/Component/Feature/Feature.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import data from '../../../public/Feature.json';
+
+const Feature = () => {
+    const [showAll, setShowAll] = useState(false);
+
+    const handleClick = () => {
+        setShowAll(true);
+    }
+
+    const jobData = showAll ? data : data.slice(0, 4);
+
+    return (
+        <div>
+            <div className='mt-20'>
+                <h1 className='font-bold text-5xl'>Featured Jobs</h1>
+                <p className='mt-3'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div className='grid md:grid-cols-2 gap-4 mt-6'>
+                {jobData.map(job => (
+                    <div key={job.id} className='border p-6 rounded text-left'>
+                        <img src={`/src/assets/All Images/job${job.id}.png`} alt={job.jobCategory} className='mx-auto mb-4' />
+                        <h2 className='font-semibold text-lg'>{job.jobPosition}</h2>
+                        <p>{job.companyName}</p>
+                        <p className='mt-4 text-blue-500'><span className='border p-2'>{job.jobTime[0]}</span><span className='border p-2 ms-4'>{job.jobTime[1]}</span></p>
+                        <p className='flex mt-3'> <img src={`/src/assets/Icons/Location Icon.png`} alt="" />{job.location} <img src={`/src/assets/Icons/Frame.png`} alt="" className='' /> Salary:{job.salary}</p>
+                        <button className='btn text-white bg-gradient-to-br from-[#7E90FE] to-[#9873FF] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-3 '>View Details</button>
+                    </div>
+                ))}
+            </div>
+            {!showAll && <button onClick={handleClick} className='btn text-white bg-gradient-to-br from-[#7E90FE] to-[#9873FF] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-6 py-3 text-center mt-5 '>Show More</button>}
+        </div>
+    );
+};
+
+export default Feature;
