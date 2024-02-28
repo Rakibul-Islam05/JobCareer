e0edcfce2c6d43305f8c73375ebf03fe30651489
Rakibul Islam <rakibulrakib05@gmail.com>
2024-02-28 18:46:58 +0600

    add footer and other

---
+++ src/Component/Statistics/Statistics.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceDot, Legend, Label } from 'recharts';
+
+const assignments = [
+  { number: 1, name: "Year-2000", progress: 50 },
+  { number: 2, name: "Year-2005", progress: 60 },
+  { number: 3, name: "Year-2010", progress: 35 },
+  { number: 4, name: "Year-2015", progress: 90 },
+  { number: 5, name: "Year-2020", progress: 50 },
+  { number: 6, name: "Year-2025", progress: 70 },
+  { number: 7, name: "Year-2030", progress: 80 },
+];
+
+
+const Statistics = ({ children }) => {
+  return (
+    <div>
+      <div className='text-center md:pt-20 md:pb-24 pt-8 pb-8 bg-hero'>
+        <h2 className='text-3xl md:text-5xl font-bold'>
+          {children}
+        </h2>
+      </div>
+      <div className=' mx-2 md:mx-12 mt-5 md:my-10'>
+        <ResponsiveContainer width="95%" height={500}>
+          <AreaChart data={assignments}>
+            <CartesianGrid strokeDasharray="3 3" stroke="#000000" />
+            <XAxis dataKey="name" axisLine={{ strokeWidth: 2 }} />
+            <YAxis axisLine={{ strokeWidth: 2 }} />
+            <Tooltip />
+            <Area type="monotone" dataKey="progress" strokeWidth={3} stroke="#FF6262" fill="#150f2d" />
+            <Legend verticalAlign="top" height={96} />
+            {
+              assignments.map((entry, index) => (
+                <ReferenceDot key={`dot-${index}`} x={entry.name} y={entry.progress} r={5} fill="#FF6262" stroke="#fff" shape="dot">
+                  <Label value={entry.progress} position="top" />
+                </ReferenceDot>
+              ))
+            }
+          </AreaChart>
+
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
